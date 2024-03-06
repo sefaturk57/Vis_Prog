ed982b31ed4cac9c39beefe70b8125be57050931
Cafer Osman Yıldız <caferosmanyildiz@gmail.com>
2024-03-06 15:35:06 +0300

    hafta 3

---
+++ HAFTA-3/Ders3/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Nevşehir");
+            comboBox1.Items.Add("Konya");
+            comboBox1.Items.Add("İstanbul");
+            comboBox1.Items.Add("Ankara");
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Ürgüp");
+                listBox1.Items.Add("Avanos");
+                listBox1.Items.Add("Göreme");
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Meram");
+                listBox1.Items.Add("Karatay");
+                listBox1.Items.Add("Selçuk");
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Keçiören");
+                listBox1.Items.Add("Mamak");
+                listBox1.Items.Add("Kızılay");
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Fatih");
+                listBox1.Items.Add("Bakırköy");
+                listBox1.Items.Add("Beyoğlu");
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "NEVŞEHİR") { 
+                comboBox1.SelectedIndex = 0;
+            }
+            else if (textBox1.Text == "KONYA")
+            {
+                comboBox1.SelectedIndex = 1;
+            }
+            else if (textBox1.Text == "ANKARA")
+            {
+                comboBox1.SelectedIndex = 2;
+            }
+            else if (textBox1.Text == "İSTANBUL")
+            {
+                comboBox1.SelectedIndex = 3;
+            }
+
+            //comboBox1.SelectedIndex = Convert.ToInt32(textBox1.Text);
+        }
+    }
+}
---
+++ HAFTA-3/ders3-2(parent ekli, derste işlenmedi)/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders3_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+            foreach (Control item0 in this.Controls)
+            {
+                if (item0 is ComboBox)
+                {
+                    (item0 as ComboBox).Items.Clear();
+                    (item0 as ComboBox).Items.Add("+");
+                    (item0 as ComboBox).Items.Add("-");
+                    (item0 as ComboBox).Items.Add("*");
+                    (item0 as ComboBox).Items.Add("/");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say1 = 0;
+            int say2 = 0;
+            int tot1 = 0;
+            string ids = "";
+            Control Gbox1 = ((Button)sender).Parent;
+
+            
+          
+
+            ////////////////////////////////////////////////////////////
+
+            foreach(Control item1 in this.Controls)
+            {
+                if (item1 is GroupBox)
+                {
+                    foreach (Control item2 in item1.Controls)
+                    {
+                        if ((item2 is TextBox) && (item2.Parent == Gbox1) && (item2.TabIndex == 21))
+                        {
+                            say1 = Convert.ToInt32((item2 as TextBox).Text);
+                            Console.WriteLine(say1);
+                        }
+                        else if ((item2 is TextBox) && (item2.Parent == Gbox1) && (item2.TabIndex == 31))
+                        {
+                            say2 = Convert.ToInt32((item2 as TextBox).Text);
+                            Console.WriteLine(say2);
+                        }
+                    }
+                   
+
+                }
+            }
+
+            ///////////////////
+            foreach (Control item4 in this.Controls)
+            {
+                if (item4 is ComboBox)
+                {
+                    ids = (item4 as ComboBox).Text;
+                    if (ids == "+")
+                    {
+                        tot1 = say1 + say2;
+
+
+                    }else if (ids == "-")
+                    {
+                        tot1 = say1 - say2;
+
+                    }
+                    else if (ids == "*")
+                    {
+                        tot1 = say1 * say2;
+
+                    }
+                    else if (ids == "/")
+                    {
+                        tot1 = say1 / say2;
+
+                    }
+
+
+
+
+                }
+
+                ///////////////////////
+                foreach(Control item5 in this.Controls)
+                {
+                    if ( item5 is GroupBox)
+                    {
+                        foreach(Control item6 in item5.Controls) 
+                        {
+                            if(item6 is Label)
+                            {
+                                (item6 as Label).Text = Convert.ToString(tot1);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+
+
+        }
+    }
+}
